eae2ceeecbc492497c91156a63a867c0b8a77718
cooi123 <calebooi@hotmail.com>
2025-05-28 18:03:16 +1000

    fix home page header redirect

--- src/components/layout/AppHeader.tsx
+++ src/components/layout/AppHeader.tsx
@@ -31,7 +31,7 @@ const AppHeader: React.FC = () => {
   return (
     <Header aria-label="Agent as Service">
       <SkipToContent />
-      <HeaderName href="/" prefix="">
+      <HeaderName href="/agents-manager" prefix="Agent as Service">
         Agent as Service
       </HeaderName>
       <HeaderNavigation aria-label="Main Navigation">
